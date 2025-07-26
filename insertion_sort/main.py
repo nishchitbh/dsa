a9e799e50eebb5b415812a720b29b0b85e7494e8
Nishchit Bhandari <nishchitbhandari2061@gmail.com>
2025-07-26 20:57:59 +0545

    feat: implementation of insertion sort

--- insertion_sort/main.py
+++ insertion_sort/main.py
@@ -1,1 +1,29 @@
 import random
+
+
+def generate_array(size):
+    generated = list(range(size))
+    random.shuffle(generated)
+    return generated
+
+
+def sort(array):
+    print(array)
+    for i in range(1, len(array)):
+        selected_card = array[i]
+        print(selected_card)
+        j = i - 1
+        while j > -1 and array[j] > selected_card:
+            array[j + 1] = array[j]
+            j = j - 1
+        array[j + 1] = selected_card
+    return array
+
+
+def runner():
+    array = generate_array(25).copy()
+    sorted_array = sort(array)
+    return sorted_array
+
+
+print(runner())
